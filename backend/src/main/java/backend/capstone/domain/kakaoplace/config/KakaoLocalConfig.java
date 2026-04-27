6c297c84b012d0e82f4fee9f23f1563925f187e7
zie-ning <jeeun03@gmail.com>
2026-04-27 09:59:31 +0900

    refactor #6: global 패키지에 있던 KakaoLocalConfig kakaoplace 하위로 이동

--- backend/src/main/java/backend/capstone/domain/kakaoplace/config/KakaoLocalConfig.java
+++ backend/src/main/java/backend/capstone/domain/kakaoplace/config/KakaoLocalConfig.java
@@ -1,4 +1,4 @@
-package backend.capstone.global.config;
+package backend.capstone.domain.kakaoplace.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
